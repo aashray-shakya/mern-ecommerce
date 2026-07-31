75317a95a5e1f69fe749e977b976cb47bcdf22a6
Aashray Shakya <aashrayshakya0@protonmail.com>
2026-07-31 08:45:34 +0000

    2026-07-31

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");  
 
 dotenv.config();
 connectDB();
@@ -23,6 +24,7 @@ app.use("/api/cart", cartRoutes);
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
